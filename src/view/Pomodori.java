494084937c54acc00dce6101dc0a7ff603a2cd7d
Morvana Bonin <morvanabonin@gmail.com>
2016-12-02 21:34:23 -0200

    WIP - caraio da chamada do JavaFX não funciona.

--- src/view/Pomodori.java
+++ src/view/Pomodori.java
@@ -1,19 +1,45 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Pomodori extends Application {
 
+    private AnchorPane root;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        TimerController teste = new TimerController();
-        teste.play();
+        /*TimerController teste = new TimerController();
+        teste.play();*/
+
+        /*FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("pomodori.fxml"));
+        loader.setRoot(root);
+        root = loader.load();
+
+        // Show the scene containing the root layout.
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();*/
+        Parent root = FXMLLoader.load(getClass().getResource("pomodori.fxml"));
+
+
+        Scene scene = new Scene(root);
+        scene.setRoot(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        TimerController teste = new TimerController();
-        teste.play();
-        //launch(args);
+        /*TimerController teste = new TimerController();
+        teste.play();*/
+        launch(args);
     }
 }
+
+
--- src/view/TimerController.java
+++ src/view/TimerController.java
@@ -3,12 +3,59 @@ package view;
 import dao.TaskDao;
 import entity.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 public class TimerController {
 
+    /** Menus */
     @FXML
-    public TextField btnPlay;
+    public Button btnPlay;
+
+    /** Report MenuButton and SubItems */
+    @FXML
+    public MenuButton menuReport;
+
+    @FXML
+    public MenuItem today;
+
+    @FXML
+    public MenuItem sevenDays;
+
+    @FXML
+    public MenuItem fifteenDays;
+
+    @FXML
+    public MenuItem thirtyDays;
+
+    @FXML
+    public Button btnAbout;
+
+    /** Home panel */
+    @FXML
+    public Pane paneHome;
+    
+    @FXML
+    public Label taskTitle;
+
+    @FXML
+    public TextField taskName;
+
+    @FXML
+    public  ProgressIndicator progressTimer;
+
+    @FXML
+    public Button btnStop;
+
+    @FXML
+    public Button btnHome;
+
+    /** Aqui a gambi por não conhecer bem o JavaFX, mas curti minha solução *-* */
+    @FXML
+    public Pane paneReport;
+    
+    @FXML
+    public Pane paneAbout;
 
     public boolean play() {
         Task task = new Task();
